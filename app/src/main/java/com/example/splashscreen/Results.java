e1268cfd5f35bc6b67f6532b4722082db5e1a877
Denis <denismamolo@virgilio.it>
2022-09-13 21:07:09 +0200

    fix results context

--- app/src/main/java/com/example/splashscreen/Results.java
+++ app/src/main/java/com/example/splashscreen/Results.java
@@ -1,9 +1,6 @@
 package com.example.splashscreen;
 
-import android.app.Service;
 import android.content.Context;
-import android.content.Intent;
-import android.os.IBinder;
 
 
 import com.example.splashscreen.database.DataEcg;
@@ -24,39 +21,56 @@ public class Results {
 
     private int id;
     private int uf;
-    private float data[];
+
+    private float interval[] = new float[2];
+    private float time_pico[] = new float[2];
+
     private long n_data;
+
+    private int index = 0;                     // Indici di lettura
+    private int new_index = 0;                 //
+
 
     public Results (Context context) {
         this.context = context;
     }
 
-    public void start_analisy() {
+    public int start_analisy() {
 
-        load_data();
+        first_load_data();
+
+        if (n_data<100) {
+            System.out.print("Dati insufficenti");
+            return 1;
+        }
+
         linear_interpolation();
         detrend();
         ht();
         htfilt();
 
+        return 0;
+
     };
 
-    private void load_data() {
+    private void first_load_data() {     //restituisce x e y per l'interpolazione
 
-        data[1] = read_value();
+        time_pico[0] = read_value();
+        interval[0] = read_value();
+        time_pico[0] += interval[0];
+
+        interval[1] = read_value();
+        time_pico[1] += interval[1];
 
     };
 
 
     private float read_value() {
 
-
-        int index = 0;
-        int new_index = 0;
         float value;
 
-        n_data = getDatabaseManager().noteModel().loadNote(id).number_data;
-        String data = getDatabaseManager().noteModel().loadNote(id).data;
+        n_data = getDatabaseManager().noteModel().loadNote(id).number_data;   // Lettura dati dal database
+        String data = getDatabaseManager().noteModel().loadNote(id).data;     //
 
         index = data.indexOf(data,new_index);
         value = Float.parseFloat(data.substring(new_index,index+2));
@@ -68,17 +82,14 @@ public class Results {
 
     private void linear_interpolation() {
 
-        float[] x = new float[2];
-        float[] y = new float[2];
+
 
 
         //A ogni chiamata restituisco un dato
 
     }
 
-    private void detrend() {
-
-    }
+    private void detrend() {}
 
     private void ht() {}
 
