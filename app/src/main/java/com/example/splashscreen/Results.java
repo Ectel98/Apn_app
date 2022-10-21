ae62889c37602c7214006caeacd7a24d8994c5d2
Denis <denismamolo@virgilio.it>
2022-10-21 16:50:39 +0200

    New functions in Results and fix class to pass data as objects in functions

--- app/src/main/java/com/example/splashscreen/Results.java
+++ app/src/main/java/com/example/splashscreen/Results.java
@@ -11,6 +11,8 @@ package com.example.splashscreen;
 import android.content.Context;
 import com.example.splashscreen.database.DataEcgDatabase;
 
+import java.util.Arrays;
+
 public class Results {
 
     private final Context context;
@@ -27,11 +29,22 @@ public class Results {
     private long id;
 
     static class parameters {
+
         public float[] interval;
         public float[] time_pico;
+
+        public float[] time;
+        public float[] ampl;
+        public float[] omega;
+
         public parameters(int dimension) {
             interval = new float[dimension];
             time_pico = new float[dimension];
+        }
+        public parameters(int dimension,int dimension2,int dimension3) {
+            time = new float[dimension];
+            ampl = new float[dimension2];
+            omega = new float[dimension3];
         }
     }
 
@@ -48,6 +61,10 @@ public class Results {
 
     private int index = 0;                     // Indici di lettura
     private int new_index = 0;                 //
+
+
+    private float[] ampl;
+    private float[] omega;
 
 
     public Results(Context context) {
@@ -250,9 +267,157 @@ public class Results {
 
     private void ht() { //Hilbert transform
 
+        final int lfilt = 128;
+
+        int npt;
+
+        //time -> time_pico
+        //x -> interval
+        final float[] xh = new float[index_interp];
+        ampl = new float[index_interp];
+        final float[] phase = new float[index_interp];
+        omega = new float[index_interp];
+        final float[] hilb = new float[lfilt+1];
+        final float pi, pi2;
+        float xt, xht, yt;
+
+        pi = (float) Math.PI;
+        pi2 = 2*pi;
+
+        for (int i=1; i<=lfilt; i++) {
+            hilb[i] = (float) (1 / ((i - lfilt / 2) - 0.5) / pi);
+        }
+
+        npt=index_interp-1;
+
+        for (int l=1; l<=npt-lfilt+1; l++) {
+            yt = 0;
+            for (int i=1; i<=lfilt; i++)
+                yt = yt+smooth.interval[l+i-1]*hilb[lfilt+1-i];
+            xh[l] = yt;
+        }
+
+        /* shifting lfilt/1+1/2 points */
+        for (int i=1; i<=npt-lfilt; i++) {
+            xh[i] = (float) 0.5*(xh[i]+xh[i+1]);
+        }
+        for (int i=npt-lfilt; i>=1; i--)
+            xh[i+lfilt/2]=xh[i];
+
+        /* writing zeros */
+        for (int i=1; i<=lfilt/2; i++) {
+            xh[i] = 0;
+            xh[npt+1-i] = 0;
+        }
+
+
+        /* Ampl and phase */
+        for (int i=lfilt/2+1; i<=npt-lfilt/2; i++) {
+            xt = smooth.interval[i];
+            xht = xh[i];
+            ampl[i] = (float)Math.sqrt(xt*xt+xht*xht);
+            phase[i] = (float)Math.atan2(xht ,xt);
+            if (phase[i] < phase[i-1])
+                omega[i] = phase[i]-phase[i-1]+pi2;
+            else
+                omega[i] = phase[i]-phase[i-1];
+        }
+
+        for (int i=lfilt/2+2; i<=npt-lfilt/2; i++) {
+            omega[i] = omega[i] / pi2;
+        }
+
     }
 
-    private void htfilt() {}
+    private void htfilt() {
+
+        final int win=0;
+
+        float x[],y[],sx[],sy[];
+
+        x = y = sx = sy =new float[win];
+
+        int i,j,hwin;
+
+        //Carica un numero di dati pari a win
+        // time, x = amp, y = omega
+
+        if (++i >= win)
+            i = 0;
+        j = hwin = win/2 -1;
+
+        for (int k=0; k<win; k++) {
+            sx[k] = x[k];
+            sy[k] = y[k];
+        }
+
+        Arrays.sort(sx);
+        Arrays.sort(sy);
+
+        printf("%g %g %g\n", time[j], sx[hwin], sy[hwin]);
+
+        while (scanf("%lf %lf %lf", &time[i], &x[i], &y[i]) == 3) {
+
+            if (++i >= win)
+                i = 0;
+            if (++j >= win)
+                j = 0;
+
+            for (int k=0; k<win; k++) {
+                sx[k] = x[k];
+                sy[k] = y[k];
+            }
+
+            Arrays.sort(sx);
+            Arrays.sort(sy);
+            printf("%g %g %g\n", time[j], sx[hwin], sy[hwin]);
+
+            ampl = sx;
+            omega = sy;
+
+        }
+
+    }
+
+    private void amp_norm() {
+
+        float av_amp,av;
+
+        av = av_amp = 0;
+
+        for (int i = 0; i< ampl.length;i++) {
+            av_amp += ampl[i];
+        }
+
+        av = av_amp/ampl.length;
+
+        for (int i = 0; i< ampl.length;i++) {
+            ampl[i] = ampl[i]/av;
+        }
+
+    }
+
+    private void ht_min_thr() {
+
+        double max,min,mid,thres;
+        float[] ord_ampl= ampl;
+
+        Arrays.sort(ord_ampl);
+
+        min = ord_ampl[0];
+        max = ord_ampl[ord_ampl.length-1];
+
+        mid = (max + min)/2;
+
+        thres = (-0.555 + 1.3*(mid+1)/2);
+
+    }
+
+    private void st_deviation() {}
+
+    private void mean() {}
+
+    private void detected() {}
 
 }
 
